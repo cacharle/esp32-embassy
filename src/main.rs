5a37f1a5d6761b9bdd51fd3594ccdcecd3171458
Charles Cabergs <me@cacharle.xyz>
2024-05-21 15:04:20 +0200

    Signal

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use core::borrow::BorrowMut;
 use static_cell::StaticCell;
 
 use esp_println::println;
@@ -17,54 +16,23 @@ use esp_hal::{
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 
-use embassy_sync::channel::{Channel, Receiver, Sender};
-use embassy_sync::pubsub::{PubSubChannel, Publisher, Subscriber};
-use embassy_sync::blocking_mutex::raw::{NoopRawMutex};
+use embassy_sync::signal::{Signal};
+// use embassy_sync::blocking_mutex::raw::{NoopRawMutex};
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 
 // embassy futures
 // embassy executor
 // embassy sync
 
-// Channel<NoopRawMutex, u32, 3>
-
-// static CHANNEL: StaticCell<Channel::<NoopRawMutex, u32, 3>> = StaticCell::new();
-
-const SUBS: usize = 100;
-const PUBS: usize = 100;
-type U32PubSub = PubSubChannel<NoopRawMutex, u32, 1, SUBS, PUBS>;
 
 #[embassy_executor::task]
-async fn print_count1(id: i32, pubsub: &'static U32PubSub) {
-    let mut sub = pubsub.subscriber().unwrap();
+async fn print_count1(signal: &'static Signal<CriticalSectionRawMutex, u32>) {
     loop {
-        Timer::after(Duration::from_millis(3000)).await;
-        let n = sub.next_message().await;
-        println!("received in {}: {:?}", id, n);
+        let n = signal.wait().await;
+        println!("received: {:?}", n);
+        Timer::after(Duration::from_millis(200)).await;
     }
 }
-
-#[embassy_executor::task]
-async fn print_count2(id: i32, pubsub: &'static U32PubSub) {
-    let mut sub = pubsub.subscriber().unwrap();
-    loop {
-        Timer::after(Duration::from_millis(2000)).await;
-        let n = sub.next_message().await;
-        println!("received in {}: {:?}", id, n);
-    }
-}
-
-
-#[embassy_executor::task]
-async fn produce_good_numbers(pubsub: &'static U32PubSub) {
-    let pub1 = pubsub.publisher().unwrap();
-    let mut counter = 1000;
-    loop {
-        pub1.publish(counter).await;
-        counter += 1;
-        Timer::after(Duration::from_millis(10)).await;
-    }
-}
-
 
 #[main]
 async fn main(spawner: Spawner) -> ! {
@@ -75,20 +43,14 @@ async fn main(spawner: Spawner) -> ! {
     let timg0 = esp_hal::timer::TimerGroup::new_async(peripherals.TIMG0, &clocks);
     embassy::init(&clocks, timg0);
 
-    static PUBSUB: StaticCell<U32PubSub> = StaticCell::new();
-    let pubsub = PUBSUB.init(PubSubChannel::new());
-
-
-    spawner.spawn(print_count1(1, pubsub)).unwrap();
-    spawner.spawn(print_count2(2, pubsub)).unwrap();
-    spawner.spawn(produce_good_numbers(pubsub)).unwrap();
-
-    let pub0 = pubsub.publisher().unwrap();
+    static SIGNAL: StaticCell<Signal<CriticalSectionRawMutex, u32>> = StaticCell::new();
+    let signal = SIGNAL.init(Signal::new());
+    spawner.spawn(print_count1(signal)).unwrap();
 
     let mut counter = 0;
     loop {
-        pub0.publish(counter).await;
+        signal.signal(counter);
         counter += 1;
-        Timer::after(Duration::from_millis(10)).await;
+        Timer::after(Duration::from_millis(100)).await;
     }
 }
