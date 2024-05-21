91df707cbfcdacb7eb1c061df4f98fe3525ca09f
Charles Cabergs <me@cacharle.xyz>
2024-05-21 16:43:46 +0200

    Task pool_size

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,26 @@ use esp_hal::{
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-
+use embassy_sync::channel::{Channel, Receiver, Sender};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 
 // embassy executor
 
-use embassy_futures::{join::join, select::{Either, select}};
-use embassy_time::Ticker;
+struct Message {
+    count: u32,
+    id: u32,
+}
+
+#[embassy_executor::task(pool_size = 10)]
+async fn sending_numbers(id: u32, sender: Sender<'static, NoopRawMutex, Message, 3>) {
+    let mut counter = 0;
+    loop {
+        let message = Message { count: counter, id };
+        sender.send(message).await;
+        counter += 1;
+        Timer::after_millis(500).await;
+    }
+}
 
 #[main]
 async fn main(spawner: Spawner) -> ! {
@@ -31,12 +45,21 @@ async fn main(spawner: Spawner) -> ! {
     let timg0 = esp_hal::timer::TimerGroup::new_async(peripherals.TIMG0, &clocks);
     embassy::init(&clocks, timg0);
 
-    let mut ticker1 = Ticker::every(Duration::from_millis(1000));
-    let mut ticker2 = Ticker::every(Duration::from_millis(1500));
+    static CHANNEL: StaticCell<Channel::<NoopRawMutex, Message, 3>> = StaticCell::new();
+    let channel = CHANNEL.init(Channel::new());
+    let sender = channel.sender();
+    let receiver = channel.receiver();
+
+    spawner.spawn(sending_numbers(1, sender)).unwrap();
+    spawner.spawn(sending_numbers(2, sender)).unwrap();
+    spawner.spawn(sending_numbers(3, sender)).unwrap();
+
     loop {
-        match select(ticker1.next(), ticker2.next()).await {
-            Either::First(_) => println!("first future finished"),
-            Either::Second(_) => println!("second future finished"),
+        match receiver.receive().await {
+            Message { count: counter, id: 1 } => println!("first task: {}", counter),
+            Message { count: counter, id: 2 } => println!("second task: {}", counter),
+            Message { count: counter, id: 3 } => println!("third task: {}", counter),
+            _ => panic!("no id"),
         }
     }
 }
