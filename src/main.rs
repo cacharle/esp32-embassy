ec27cd00033d5a027c5c1d3bdb487fef10f08e53
Charles Cabergs <me@cacharle.xyz>
2024-05-22 14:58:31 +0200

    UART+I2C

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,30 @@ use esp_println::println;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
-    peripherals::{Peripherals},
+    peripherals::{Peripherals, UART1},
     prelude::*,
     embassy,
-    // uart::{Uart, TxRxPins, UartRx, config::{Config, AtCmdConfig}},
+    uart::{Uart, TxRxPins, UartRx, config::{Config, AtCmdConfig}},
     gpio::IO,
-    // Async,
+    Async,
     i2c::{I2C},
     delay::Delay,
 };
 use embassy_executor::Spawner;
 use embassy_time::{Ticker, Duration, Timer};
-// // use embassy_sync::channel::{Channel, Sender};
-// use embassy_sync::pipe::{Pipe, Writer};
-// use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::pipe::{Pipe, Writer};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 
-// const BUF_SIZE: usize = 0x7f;
+const BUF_SIZE: usize = 0x7f;
 
-// #[embassy_executor::task]
-// async fn reader(mut rx: UartRx<'static, UART1, Async>, pipe_writer: Writer<'static, NoopRawMutex, BUF_SIZE>) {
-//     loop {
-//         let mut buf: [u8; BUF_SIZE] = [0x00; BUF_SIZE];
-//         let read_size = rx.read_async(&mut buf).await.unwrap();
-//         pipe_writer.write(&buf[..read_size]).await;
-//     }
-// }
+#[embassy_executor::task]
+async fn reader(mut rx: UartRx<'static, UART1, Async>, pipe_writer: Writer<'static, NoopRawMutex, BUF_SIZE>) {
+    loop {
+        let mut buf: [u8; BUF_SIZE] = [0x00; BUF_SIZE];
+        let read_size = rx.read_async(&mut buf).await.unwrap();
+        pipe_writer.write(&buf[..read_size]).await;
+    }
+}
 
 use liquidcrystal_i2c_rs::{Backlight, Display, Lcd};
 
@@ -47,6 +46,10 @@ async fn main(spawner: Spawner) -> ! {
     let timg0 = esp_hal::timer::TimerGroup::new_async(peripherals.TIMG0, &clocks);
     embassy::init(&clocks, timg0);
 
+    static PIPE: StaticCell<Pipe<NoopRawMutex, BUF_SIZE>> = StaticCell::new();
+    let pipe = PIPE.init(Pipe::new());
+    let (pipe_reader, pipe_writer) = pipe.split();
+
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut i2c = I2C::new_async(
         peripherals.I2C0,
@@ -56,20 +59,42 @@ async fn main(spawner: Spawner) -> ! {
         &clocks,
     );
 
-    let mut ticker = Ticker::every(Duration::from_millis(100));
+    let pins = TxRxPins::new_tx_rx(io.pins.gpio18, io.pins.gpio5);
+    let mut uart = Uart::new_async_with_config(
+        peripherals.UART1,
+        Config::default(),
+        Some(pins),
+        &clocks,
+    );
+    uart.set_at_cmd(AtCmdConfig::new(None, None, None, b'\r', None));
+    uart.set_rx_fifo_full_threshold(BUF_SIZE as u16).unwrap();
+    let (mut tx, rx) = uart.split();
+    spawner.must_spawn(reader(rx, pipe_writer));
+
+    Timer::after_millis(5_000).await;
+    let mut delay = Delay::new(&clocks);
+    let mut lcd = match Lcd::new(&mut i2c, LCD_ADDRESS, &mut delay) {
+        Ok(lcd) => lcd,
+        Err(e) => {
+            panic!("Error LCD: {:?}", e);
+        }
+    };
+
+    // let mut ticker = Ticker::every(Duration::from_millis(100));
     loop {
-        let mut delay = Delay::new(&clocks);
-        let mut lcd = match Lcd::new(&mut i2c, LCD_ADDRESS, &mut delay) {
-            Ok(lcd) => lcd,
-            Err(e) => {
-                println!("Error LCD: {:?}", e);
-                continue;
-            }
-        };
+        let mut buf: [u8; BUF_SIZE] = [0x00; BUF_SIZE];
+        let n = pipe_reader.read(&mut buf).await;
+        let s = core::str::from_utf8(&buf[..n]).unwrap().trim();
+        println!("Read: {}", s);
+        // tx.write_async(b"> ").await.unwrap();
+        // tx.write_async(&buf).await.unwrap();
+        // tx.write_async(b"\r\n").await.unwrap();
+        // tx.flush_async().await.unwrap();
+
         lcd.set_display(Display::On).unwrap();
         lcd.set_backlight(Backlight::On).unwrap();
         lcd.clear().unwrap();
-        lcd.print("Hello World!").unwrap();
-        ticker.next().await;
+        lcd.print(s).unwrap();
+        // ticker.next().await;
     }
 }
