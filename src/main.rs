150ebd52ee1c584205341875f4091e0eb2f84a3b
Charles Cabergs <me@cacharle.xyz>
2024-05-22 11:35:43 +0200

    UART echo

--- src/main.rs
+++ src/main.rs
@@ -8,37 +8,27 @@ use esp_println::println;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
-    peripherals::Peripherals,
+    peripherals::{Peripherals, UART1},
     prelude::*,
     embassy,
+    uart::{Uart, TxRxPins, UartRx, config::{Config, AtCmdConfig}},
+    gpio::IO,
+    Async,
 };
-
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
-use embassy_sync::channel::{Channel, Receiver, Sender};
+use embassy_time::{Ticker, Duration};
+// use embassy_sync::channel::{Channel, Sender};
+use embassy_sync::pipe::{Pipe, Writer};
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 
-// embassy executor
+const BUF_SIZE: usize = 0x7f;
 
-#[derive(Debug)]
-struct Message {
-    count: u32,
-    id: u32,
-}
-
-#[embassy_executor::task(pool_size = 10)]
-async fn sending_numbers(id: u32, sender: Sender<'static, NoopRawMutex, Message, 3>) {
-    if id < 10 {
-        let spawner = Spawner::for_current_executor().await;
-        spawner.must_spawn(sending_numbers(id + 10, sender));
-    }
-
-    let mut counter = 0;
+#[embassy_executor::task]
+async fn reader(mut rx: UartRx<'static, UART1, Async>, pipe_writer: Writer<'static, NoopRawMutex, BUF_SIZE>) {
     loop {
-        let message = Message { count: counter, id };
-        sender.send(message).await;
-        counter += 1;
-        Timer::after_millis(500).await;
+        let mut buf: [u8; BUF_SIZE] = [0x00; BUF_SIZE];
+        let read_size = rx.read_async(&mut buf).await.unwrap();
+        pipe_writer.write(&buf[..read_size]).await;
     }
 }
 
@@ -51,17 +41,35 @@ async fn main(spawner: Spawner) -> ! {
     let timg0 = esp_hal::timer::TimerGroup::new_async(peripherals.TIMG0, &clocks);
     embassy::init(&clocks, timg0);
 
-    static CHANNEL: StaticCell<Channel::<NoopRawMutex, Message, 3>> = StaticCell::new();
-    let channel = CHANNEL.init(Channel::new());
-    let sender = channel.sender();
-    let receiver = channel.receiver();
+    static PIPE: StaticCell<Pipe<NoopRawMutex, BUF_SIZE>> = StaticCell::new();
+    let pipe = PIPE.init(Pipe::new());
+    let (pipe_reader, pipe_writer) = pipe.split();
 
-    spawner.must_spawn(sending_numbers(1, sender));
-    spawner.must_spawn(sending_numbers(2, sender));
-    spawner.must_spawn(sending_numbers(3, sender));
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let pins = TxRxPins::new_tx_rx(io.pins.gpio18, io.pins.gpio5);
+    let mut uart = Uart::new_async_with_config(
+        peripherals.UART1,
+        Config::default(),
+        Some(pins),
+        &clocks,
+    );
+    uart.set_at_cmd(AtCmdConfig::new(None, None, None, b'\r', None));
+    uart.set_rx_fifo_full_threshold(BUF_SIZE as u16).unwrap();
+    let (mut tx, rx) = uart.split();
+    spawner.must_spawn(reader(rx, pipe_writer));
 
+    // let mut ticker = Ticker::every(Duration::from_millis(1_000));
+
+    tx.write_async(b"I repeat everything").await.unwrap();
+    tx.flush_async().await.unwrap();
     loop {
-        let v = receiver.receive().await;
-        println!("received: {:?}", v);
+        let mut buf: [u8; BUF_SIZE] = [0x00; BUF_SIZE];
+        pipe_reader.read(&mut buf).await;
+        tx.write_async(b"> ").await.unwrap();
+        tx.write_async(&buf).await.unwrap();
+        tx.write_async(b"\r\n").await.unwrap();
+        tx.flush_async().await.unwrap();
+        println!("sent bytes with UART: {:?}", buf);
+        // ticker.next().await;
     }
 }
